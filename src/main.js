237f78f725d912498694572cc19c78f0e63e9735
xiaoyu_jie <kclon29@gmail.com>
2026-02-23 19:12:34 +0800

    emm

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import './assets/main.css'
+if (import.meta.env.MODE === 'development') {
+  window.__VUE_DEVTOOLS_CONFIG__ = {
+    showWidget: false,
+  }
+}
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
